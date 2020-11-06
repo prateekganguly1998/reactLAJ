73a52e24853f6ccca28504e5e925b22543adac54
Prateek Gangopadhyay <gangopadhyay_p@India.XoriantCorp.com>
2020-11-06 22:38:57 +0530

    d1F

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 import {fetchWeather} from './service/fetchWeatherData';
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import WeatherCard from './WeatherCard';
+import './App.css'
+export default class App extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state=
+        {
+            items:[],
+            query:'',
+            weather:{}
+        }
+        this.search=this.search.bind(this);
+    }
+   async search(e)
+    {
+        if(e.key==='Enter')
+        {
+            const data=await fetchWeather(this.state.query);
+            console.log(data);
+            this.setState({weather:data});
+        }
+        this.forceUpdate();
+        
+    }
+    
+    componentDidUpdate()
+    {
+        if(this.state.weather.list)
+        {
+            for(let i=0;i<this.state.weather.list.length;i=i+8)
+            {
+                this.state.items.push(<WeatherCard weather={this.state.weather.list[i]} location={this.state.weather.city}/>)
+            }
+            console.log(this.state.items);
+           
+        }
+     
+       
+    }
+    
+    render() {
+        
+        return (
+            <div className="main-container">
+                 <input type="text"className="search"placeholder="Search..."value={this.state.query}onChange={(e) => this.setState({query:e.target.value})}onKeyPress={this.search}/>
+                <div style={{display:"flex"}}>
+                    {this.state.items}
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
